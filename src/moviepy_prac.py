fb04e09cb57be24d1d6ad8554c4801e2dddc260b
yo16 <xxyo1xx@gmail.com>
2024-05-29 00:05:33 +0900

    changed out file name

--- src/moviepy_prac.py
+++ src/moviepy_prac.py
@@ -1,4 +1,5 @@
 from moviepy.editor import *
+from pathlib import Path
 
 def main(movie_file):
     clip1 = VideoFileClip(movie_file)
@@ -7,9 +8,14 @@ def main(movie_file):
     top = 124
     clip2 = (clip1.fx(vfx.crop, x1=0, y1=top, x2=320, y2=top+220))
 
-    clip2.write_videofile('./data/out.mp4')
-    clip2.write_gif('./data/out.gif', fps=15)
+    # 保存
+    file_path = Path(movie_file)
+    tmp = file_path.parent / f"{file_path.stem}_out.mp4"
+    print(tmp)
+    
+    clip2.write_videofile(str(file_path.parent / f"{file_path.stem}_out.mp4"))
+    clip2.write_gif(str(file_path.parent / f"{file_path.stem}_out.gif"), fps=20)
 
 if __name__=='__main__':
-    movie_file = './data/mv1.mp4'
+    movie_file = './data/mv2.mp4'
     main(movie_file)
